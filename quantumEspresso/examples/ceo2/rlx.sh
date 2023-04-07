6436580b328591bec9bec9c2a2559f75cd094da8
ecmLab <howietu@berkeley.edu>
2023-04-07 10:53:01 -0400

    packages

---
+++ quantumEspresso/examples/ceo2/rlx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#
+
+prefix="ceo2"
+jobfile="slurm_rlx.sh"
+
+jobname=$prefix-rlx
+outfile=$prefix-rlx.o
+errfile=$prefix-rlx.e
+
+spack load quantum-espresso/7hk6bwf
+
+sbatch -J $jobname -o $outfile -e $errfile $jobfile
---
+++ quantumEspresso/examples/pfoa/slurm_rlx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#
+
+#SBATCH --mail-user tek5268@rit.edu
+#SBATCH --mail-type=ALL
+
+#SBATCH -t 0-12:0:0
+
+#SBATCH -A 2dptheory -p tier3
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --mem-per-cpu=3g
+
+export ESPRESSO_PSEUDO=/home/tek5268/qe_potentials
+printenv ESPRESSO_PSEUDO
+
+srun -n 2 pw.x < pfoa.relax.in
