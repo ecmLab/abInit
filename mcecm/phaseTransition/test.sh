5ad3e2f3f4c33bb0a47fa0fda198b6424b888bd0
ecmLab <howietu@berkeley.edu>
2025-01-01 14:49:38 -0500

    010125_mc

--- mcecm/phaseTransition/test.sh
+++ mcecm/phaseTransition/test.sh
@@ -6,15 +6,17 @@
 #SBATCH --partition=tier3
 #
 # Processors:
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=32
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
 #
 # Wall clock limit:
 #SBATCH --time=01:00:00
-#SBATCH --mem=50g
+#SBATCH --mem=10g
 
 
 ## Commands to run:
 #spack load lammps@20230208 /cuxhkce
+#srun lmp -log none -in mc_run.in
+#srun python mc_mpi.py
 #mpirun --mca btl ^sm -n 4 python mc_mpi.py
-mpirun -n 64 python mc_mpi.py
+mpirun -n 16 python mc_mpi.py
