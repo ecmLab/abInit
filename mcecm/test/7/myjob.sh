1a0d5ea6e970c4fed43d5f3994a6d5cf269fa9ba
ecmLab <howietu@berkeley.edu>
2025-01-08 07:01:13 -0500

    010825_mc

--- mcecm/test/7/myjob.sh
+++ mcecm/test/7/myjob.sh
@@ -10,7 +10,7 @@
 #SBATCH --ntasks-per-node=32
 #
 # Wall clock limit:
-#SBATCH --time=24:00:00
+#SBATCH --time=8:00:00
 #SBATCH --mem=50g
 
 
--- mcecm/test/9/sporc_mpi6.py
+++ mcecm/test/9/sporc_mpi6.py
@@ -154,7 +154,7 @@ def main():
     stiffness_tensor = precompute_stiffness_tensor(anisoPar)
     q_grid, B = precompute_reciprocal_space_and_kernel(lattice_size, stiffness_tensor)
 
-    for temp_index, temperature in enumerate(np.linspace(10, 1, 10)):
+    for temp_index, temperature in enumerate(np.linspace(2, 0.2, 10)):
 
         terminate_flag = False
         zero_move_steps = 0
