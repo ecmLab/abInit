5336f275d5fd1246635e3da42fb54f22a0013784
ecmLab <howietu@berkeley.edu>
2025-05-27 16:28:36 -0400

    052725_mos2

--- mdecm/mos2_reactiveMembrane/r50/run.sh
+++ mdecm/mos2_reactiveMembrane/r50/run.sh
@@ -2,9 +2,8 @@
 # The -l above is required to get the full environment with modules
 
 #SBATCH --job-name=MoS2
-#SBATCH --account=purewater
-##SBATCH --partition=tier3
-#SBATCH --partition=debug
+#SBATCH --partition=tier3
+##SBATCH --partition=debug
 
 # displays outputs/err
 ##SBATCH --output=%x_%j.out
@@ -12,12 +11,12 @@
 #SBATCH --mail-user=slack:@qhteme
 
 #  wall-clock time for tier3
-##SBATCH -t 0-60:00:00
-#SBATCH -t 0-02:00:00
+#SBATCH -t 0-24:00:00
+##SBATCH -t 0-02:00:00
 
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=12
-#SBATCH --mem-per-cpu=20g
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem-per-cpu=500M
 
 #load the lammps module
 spack load lammps@20230802.2
