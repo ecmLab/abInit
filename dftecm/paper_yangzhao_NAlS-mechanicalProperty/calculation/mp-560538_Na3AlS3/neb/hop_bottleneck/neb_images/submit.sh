cbeea16f6c831c10395539115a5f9b33b293b9d2
ecmLab <howietu@berkeley.edu>
2026-04-03 21:31:24 -0400

    040326_NAlS

---
+++ dftecm/paper_yangzhao_NAlS-mechanicalProperty/calculation/mp-560538_Na3AlS3/neb/hop_bottleneck/neb_images/submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH --job-name=Na3AlS_bn_neb
+#SBATCH --account=membrane
+#SBATCH --partition=tier3
+#SBATCH --mail-user=slack:@qhteme
+#SBATCH -t 10:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=27
+#SBATCH --ntasks-per-node=27
+#SBATCH --mem-per-cpu=500M
+
+spack load vasp@6.3.2
+srun vasp_std
